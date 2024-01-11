9c1423bc01ae93396483e946670bab0b4c6c4cf6
DaanCarst <daanbrother@icloud.com>
2024-01-11 17:44:43 +0100

    begin toevoeg pagina

---
+++ src/routes/add/+page.svelte
@@ -0,0 +1,81 @@
+<section class="content-container">
+    <article class="tip-container">
+        <p class="tip">Tip</p>
+        <p>Voeg een bestaande url toe van de website van een partner!</p>
+    </article>
+
+    <section class="form-container">
+        <form action="">
+                <label for="name">Voeg een pagina titel toe</label>
+                <input id="name" required type="text"><br>
+            
+                <label for="url" class="url-container">Voeg een Partner URL toe</label>
+                <input id="url" required type="url">
+            <button class="add-button">Project Toevoegen</button>
+        </form>
+    </section>
+</section>
+
+<style>
+	.content-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: flex-start;
+    }
+
+    article, .form-container {
+        background-color: var(--c-container);
+        margin: .5em;
+        border-radius: 4px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        padding: 1.2em;
+    }
+
+    .form-container {
+        line-height: 2em;
+        width: 100%;
+    }
+
+    .tip {
+        color: var(--c-orange);
+        line-height: 1.5em;
+    }
+
+    .add-button {
+        border: none;
+        background-color: var(--c-modal-button);
+        color: white;
+        padding: .7em;
+        cursor: pointer;
+        text-decoration: none;
+        transition: 0.3s;
+        border-radius: 4px;
+        margin-top: 1em;
+    }
+
+    input[type=text], input[type=url] {
+        width: 100%;
+        padding: 12px 20px;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+        max-width: 700px;
+    }
+
+    .add-button:hover {
+        opacity: 0.75;
+    }
+
+
+    @media (max-width: 850px) {
+        .content-container {
+            flex-wrap: wrap;
+        }
+    }   
+
+</style>
