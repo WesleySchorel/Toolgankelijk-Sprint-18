21a4a36d023f86659ecd6396520de51fbd5f2f6b
Tolga1999 <tolgadoganer@outlook.com>
2024-01-16 14:53:10 +0100

    Partner toevoegen link
    styling voor partner toevoegen link, volgens de huisstyle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,16 @@
 	export let data;
 
 	let heading = { titel: 'Partners overzicht' };
-	console.log(data)
-	
+	console.log(data);
 </script>
 
 <Heading {heading} />
 
-<Search />
+<section>
+	<a href="/add">Partner toevoegen</a>
+	<Search />
+</section>
 
-<a href="/add">add partner</a>
 <ul>
 	{#each data.websites as website}
 		<Partner {website} />
@@ -21,6 +22,32 @@
 </ul>
 
 <style>
+	section{
+		display: flex;
+		justify-content: space-between;
+		margin: 0 0 1em 1em;
+	}
+
+	a {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 0.25em;
+		padding: 0.5em 1em;
+		color: var(--c-white);
+		background-color: var(--c-modal-button);
+		border: none;
+		font-weight: 600;
+		font-size: 1em;
+		transition: 0.3s;
+		cursor: pointer;
+		text-decoration: none;
+	}
+
+	a:hover {
+		opacity: 0.75;
+	}
+
 	ul {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(20em, 1fr));
