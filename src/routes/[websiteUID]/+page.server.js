948927dc410aed57280bf1f7d074c1b2e57d6b4e
RickVellingaa <rickvellinga2408@gmail.com>
2024-01-17 01:02:11 +0100

    Action voor verwijderen

--- src/routes/[websiteUID]/+page.server.js
+++ src/routes/[websiteUID]/+page.server.js
@@ -1,11 +1,27 @@
-import { gql } from 'graphql-request'
-import { hygraph } from '$lib/utils/hygraph.js'
+import { gql } from 'graphql-request';
+import { hygraph } from '$lib/utils/hygraph.js';
 
-import getQueryWebsite from '$lib/queries/website'
+import getQueryWebsite from '$lib/queries/website';
+import getQueryDeleteUrl from '$lib/queries/deleteUrl';
 
-export async function load({params}) {
-    const { websiteUID } = params;
-    let query = getQueryWebsite(gql, websiteUID)
-    
-    return await hygraph.request(query).websitesData
+export async function load({ params }) {
+	const { websiteUID } = params;
+	let query = getQueryWebsite(gql, websiteUID);
+
+	return await hygraph.request(query).websitesData;
 }
+
+export const actions = {
+	default: async ({ request }) => {
+		const formData = await request.formData();
+		const id = formData.get('id');
+
+		console.log(id);
+
+		let query = getQueryDeleteUrl(gql, id);
+		return await hygraph.request(query);
+
+		// Process the form data and perform actions
+		return { success: 'Verwijdert!' };
+	}
+};
