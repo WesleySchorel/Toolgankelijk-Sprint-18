4538f07537323e81c6f8122e86e7acaf88b93b2d
RickVellingaa <rickvellinga2408@gmail.com>
2024-01-23 17:05:34 +0100

    Verwijder en bewerken van Partner

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,35 @@
-import { gql } from 'graphql-request'
-import { hygraph } from '$lib/utils/hygraph.js'
+import { gql } from 'graphql-request';
+import { hygraph } from '$lib/utils/hygraph.js';
 
-import getQueryPartner from '$lib/queries/partner'
+import getQueryPartner from '$lib/queries/partner';
+import getQueryDeletePartner from '$lib/queries/deletePartner';
+import getQueryUpdatePartner from '$lib/queries/updatePartner';
 
 export async function load() {
-    let query = getQueryPartner(gql)
-    
-    return await hygraph.request(query)
+	let query = getQueryPartner(gql);
+	return await hygraph.request(query);
 }
+
+export const actions = {
+	deletePartner: async ({ request }) => {
+		const formData = await request.formData();
+		const id = formData.get('id');
+
+		console.log(id);
+
+		let query = getQueryDeletePartner(gql, id);
+		return await hygraph.request(query);
+	},
+	editPartner: async ({ request }) => {
+		const formData = await request.formData();
+		const id = formData.get('id');
+		const name = formData.get('name');
+		const slug = formData.get('slug');
+		const url = formData.get('url');
+
+		console.log(id, name, slug, url);
+
+		let query = getQueryUpdatePartner(gql, name, slug, url, id);
+		return await hygraph.request(query);
+	}
+};
